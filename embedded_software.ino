2cb87384b8b81f9a6e2a350c21d692c43565863f
schwidrowski <91542778+schwidrowski@users.noreply.github.com>
2021-10-26 14:51:25 +0200

    Add files via upload

---
+++ embedded_software.ino
@@ -0,0 +1,57 @@
+#include <rp2040_pio.h>
+#include <Adafruit_NeoPixel.h>
+
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 16 // Popular NeoPixel ring size
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
+
+int ldrValue;
+int brightness;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(5,OUTPUT);
+
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+
+}
+
+void loop() {
+  
+  ldrValue = analogRead(A0);
+
+  delay(20);
+
+  brightness = map(ldrValue, 0, 1023, 255, 0);
+  analogWrite(5, brightness);
+  Serial.println(brightness);
+
+    pixels.clear(); // Set all pixel colors to 'off'
+
+  // The first NeoPixel in a strand is #0, second is 1, all the way up
+  // to the count of pixels minus one.
+  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+
+    pixels.show();   // Send the updated pixel colors to the hardware.
+
+    delay(DELAYVAL); // Pause before next pass through loop
+
+}
